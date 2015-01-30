ea9cf44e398bb725404109b773845f76b6297455
aloisdg <aloisdegouvello@live.fr>
2015-01-30 13:53:10 +0100

    Add resizing

--- BorderlessWindow/MainWindow.xaml.cs
+++ BorderlessWindow/MainWindow.xaml.cs
@@ -20,9 +20,91 @@ namespace BorderlessWindow
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool ResizeInProcess { get; set; }
+        //private ResizeDirection Direction { get; set; }
+
+        // https://msdn.microsoft.com/en-us/library/system.windows.forms.anchorstyles.aspx
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ResizeInProcess = false;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!(Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl)))
+                return;
+            base.OnMouseLeftButtonDown(e);
+            this.Cursor = Cursors.SizeAll;
+            this.DragMove();
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Cursor = null;
+        }
+
+        private void InitResizing(object sender, MouseButtonEventArgs e)
+        {
+            var senderRect = sender as Grid;
+            if (senderRect == null) return;
+
+            //Direction = DirectionHelper.Get(senderRect.Tag.ToString());
+            ResizeInProcess = true;
+            senderRect.CaptureMouse();
+        }
+
+        private void EndResizing(object sender, MouseButtonEventArgs e)
+        {
+            var senderRect = sender as Grid;
+            if (senderRect == null) return;
+
+            //Direction = ResizeDirection.None;
+            ResizeInProcess = false;
+            senderRect.ReleaseMouseCapture();
+        }
+
+        private void Resize(object sender, MouseEventArgs e)
+        {
+            if (!ResizeInProcess) return;
+            var senderRect = sender as Grid;
+            if (senderRect == null) return;
+
+            senderRect.CaptureMouse();
+            //if (senderRect.Cursor.Equals(Cursors.SizeWE))
+            if (senderRect.Tag.ToString().Contains("Left") || senderRect.Tag.ToString().Contains("Right"))
+            {
+                var x = e.GetPosition(this).X + 5;
+                if (senderRect.Tag.ToString().Contains("Left"))
+                //if (Direction.Equals(ResizeDirection.Left))
+                {
+                    if (this.Width <= x)
+                        return;
+                    this.Width -= x;
+                    this.Left += x;
+                }
+                else
+                    if (x > 0)
+                        this.Width = x;
+            }
+            //else if (senderRect.Cursor.Equals(Cursors.SizeNS))
+            if (senderRect.Tag.ToString().Contains("Top") || senderRect.Tag.ToString().Contains("Bottom"))
+            {
+                var y = e.GetPosition(this).Y + 5;
+                if (senderRect.Tag.ToString().Contains("Top"))
+                //if (Direction.Equals(ResizeDirection.Top))
+                {
+                    if (this.Height <= y)
+                        return;
+                    this.Height -= y;
+                    this.Top += y;
+                }
+                else
+                    if (y > 0)
+                        this.Height = y;
+            }
         }
     }
 }
